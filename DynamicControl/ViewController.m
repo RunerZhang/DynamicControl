6152947cc8cc69c8e4fe811d7300f4c3e9ccc8b5
RunerZhang <cj@tan-mb.local>
2017-06-22 15:30:03 +0800

    Initial Commit

---
+++ DynamicControl/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  DynamicControl
+//
+//  Created by cj on 2017/6/22.
+//  Copyright © 2017年 r. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
